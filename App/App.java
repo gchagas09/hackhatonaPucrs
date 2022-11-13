a959964b59cd4e10ec4625117a795db3c3892b7a
Gabriel Chagas <gchagas090299@outlook.com.br>
2022-11-12 23:44:00 -0300

    feat: criadas classes app e aluno

---
+++ App/App.java
@@ -0,0 +1,12 @@
+package App;
+
+import App.Classes.Aluno;
+
+public class App {
+    public static void main(String[] args) {
+        Aluno aluno1 = new Aluno("Gabriel", "gabriel.gchagas99@edu.pucrs.br", 22204007);
+
+        System.out.println(aluno1);
+
+    }
+}
---
+++ App/Classes/Aluno.java
@@ -0,0 +1,51 @@
+package App.Classes;
+
+public class Aluno {
+    private String nome, email, time;
+    private int matricula;
+
+    public Aluno(String nome, String email, int matricula) {
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+        this.time = "<ALUNO SEM TIME>";
+    }
+
+    @Override
+    public String toString() {
+        return "Nome=" + this.nome + "\nEmail=" + this.email + "\nTime=" + this.time + "\nMatricula=" + this.matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+}
